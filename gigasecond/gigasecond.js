8e25f8d1a8eab691761cc0c9ed3bdaab961a4c76
laurelton <777443+laurelton@users.noreply.github.com>
2018-03-09 17:16:08 -0500

    Initial few exercises

---
+++ gigasecond/gigasecond.js
@@ -0,0 +1,15 @@
+'use strict';
+
+class Gigasecond {
+    constructor(date) {
+        this._start = new Date(date);
+        date.setSeconds(date.getSeconds() + 1000000000);
+        this._date = date;
+    }
+
+    date() {
+        return this._date;
+    }
+}
+
+module.exports = Gigasecond;
--- pangram/pangram.js
+++ pangram/pangram.js
@@ -3,10 +3,25 @@
 class Pangram {
     constructor(sentence) {
         this._sentence = sentence.toLowerCase();
+        this._letters = Array.from(Array(26).keys()).map((elem, indx) => { return String.fromCharCode(97 + indx); });
+    }
+
+    get letters() {        
+        return this._letters;
     }
 
     isPangram() {
+        // return this._letters.every(elem => {this._sentence.match(new RegExp(elem))});
+        for (let lttr in this._letters) {
+            let regEx = new RegExp(this._letters[lttr]);
+            let match = this._sentence.match(regEx);
 
+            if (match == null) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
